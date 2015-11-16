79806a57e3ea6b789d452653491a9a29f99f8666
nsrchemie <nsrchemie@gmail.com>
2015-11-16 14:15:33 -0800

    jquery edits

--- js/join.js
+++ js/join.js
@@ -29,29 +29,30 @@
 //JQUERY
 
 $(function() {
-    $('#signup').attr('novalidate', true);
+    $('#signup').attr('novalidate', true); // When the page loads, deactivate built-in browser validation checks
 });
 
-$('#submit').click(function(event) {
+$('#submit').click(function(event) { // When the Submit button is clicked...
 
-if ($('[name="name"]').val().length < 1) {
+if ($('[name="name"]').val().length < 1) { //Return alert if nothing entered into the name field
     window.alert("Type in your name!");
 };
 
-if ($('[name="username"]').val().length < 1) {
+if ($('[name="username"]').val().length < 1) { //Return alert if nothing entered into the username field
     window.alert("Type in your username!");
 };
 
     var emailCheck = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
-if (emailCheck.test($('[name="email"]').val()) === false) {
+if (emailCheck.test($('[name="email"]').val()) === false) { //If the email input does not match the Regular Expression
      window.alert('Type in a valid email!!!');
-     event.preventDefault();
+     event.preventDefault(); //Retain information already inputted, dont clear all fields
 };
 
-// $.get( "/javapic/gallery.html", function() {
-//     $('.tagline').html($('[name="name"]').val()
-//     );});
-var $name = $('[name="name"]').val();
-$('#signup').attr("action","gallery.html?" + $name);
+
+var $name = $('[name="name"]').val();//Retain the value of the Name input
+
+event.preventDefault(); // This allows the line below to execute without being skipped
+window.location.href = "gallery.html?" + $name; //This alters the URL because we can't use GET or POST
+
 });
